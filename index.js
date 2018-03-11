7c0a73bd21c066ec11ea6594e944c57d778f279b
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-11 16:43:11 +0100

    ALs disconnect dan stop programma

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ const microbit = require('bbc-microbit');
 console.log("De mcirobit zoeken....")
 microbit.discover((mb) => {
    console.log("Gevonden")
+
+
+    mb.on("disconnect",() => {
+       console.log("Doei");
+       //Stop de programma...
+       process.exit(0);
+    });
+
+
 });

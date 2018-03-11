1edf9affd4896c22aa6b90569a1940635e76cf21
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-11 17:09:45 +0100

    A is geluid boven

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const microbit = require('bbc-microbit');
 const samsung_TV = require('samsung-remote');
 
 
-let configuratie = new SamsungRemote({
+let configuratie = new samsung_TV({
     ip: '192.168.178.116'
 });
 
@@ -19,11 +19,28 @@ microbit.discover((mb) => {
 
     mb.on("buttonAChange",(data) => {
         console.log("A ingedrukt")
+        configuratie.send('KEY_VOLUP',(err) => {
+            if (err) {
+                console.log("Hmm er is iets mis " + err)
+            } else {
+                console.log("yay")
+            }
+
+        })
     });
 
 
     mb.on("buttonBChange",(data) => {
         console.log("B ingedrukt")
+    });
+
+
+    configuratie.isAlive((err) => {
+        if (err) {
+            console.log('TV is offline');
+        } else {
+            console.log('TV is aan!');
+        }
     });
 
 
